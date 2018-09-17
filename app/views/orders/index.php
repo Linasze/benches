ea36a56723b56c1e43882580906b0414b5c36b3d
Linas Zemgulys <zykas911@gmail.com>
2018-09-17 17:55:06 +0300

    Added footer

--- app/views/orders/index.php
+++ app/views/orders/index.php
@@ -152,6 +152,7 @@ Rūšiuoti nuo:
         </nav>
         <?php endif; ?>
 </div>
+<footer class="footer">&copy Copyright <?php echo date('Y');?></footer>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
