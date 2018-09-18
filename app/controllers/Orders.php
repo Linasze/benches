a08823f9a31be613463636a018ebe5a993ff5a94
Linas Zemgulys <zykas911@gmail.com>
2018-09-18 06:36:56 +0300

    Added unconfirmOrders button in orders controller

--- app/controllers/Orders.php
+++ app/controllers/Orders.php
@@ -89,7 +89,7 @@ class Orders extends Controller {
             <td><?php echo $order->email;?></td>
             <td><?php echo $order->phone_number;?></td>
             <td><?php echo $order->address . " " . $order->zip;?></td>
-            <td class="text-center"><?php echo ($order->status == 1) ? "<i class='fas fa-check'></i>" : 
+            <td class="text-center"><?php echo ($order->status == 1) ? "<a data-toggle='confirmation' data-title=Atšaukti? href='$urlroot/orders/unconfirmOrder/$order->id'><i class='fas fa-check'></i></a>" : 
         "<a data-toggle='confirmation' data-title=Įvykdytas? href='$urlroot/orders/confirmOrder/$order->id'><i title='Patvirtinti' class='fas fa-times'></i></a>";?></td>
             <td><?php echo date("y-m-d H:i", strtotime($order->created_at));?></td>
            </tr>               
